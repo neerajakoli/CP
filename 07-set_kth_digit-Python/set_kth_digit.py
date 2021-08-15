98d427d13684062a665d9cc40bf0e2d7c4e011ba
neerajakoli <72116826+neerajakoli@users.noreply.github.com>
2021-08-15 09:14:18 +0530

    passed

--- 07-set_kth_digit-Python/set_kth_digit.py
+++ 07-set_kth_digit-Python/set_kth_digit.py
@@ -3,9 +3,17 @@
 # single digit (between 0 and 9 inclusive). This function returns the number n with 
 # the kth digit replaced with d. Counting starts at 0 and goes right-to-left, 
 # so the 0th digit is the rightmost digit. 
-
+# (468, 1, 3, 418)
 
 
 def fun_set_kth_digit(n, k, d):
-		return 0
+	c=abs(n)
+	a=str(c)[::-1]
+	if(k>len(a)-1):
+		e=str(a)+str(d)
+	else:
+		e=str(a).replace(a[k],str(d))
+	if(n<0):
+		return int(e[::-1])*(-1)
+	return int(e[::-1])
 
