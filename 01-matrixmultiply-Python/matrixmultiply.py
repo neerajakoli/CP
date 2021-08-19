4e80a32400cb0f510ccc1f952a52535e92454071
neerajakoli <neeraja.kolisetty@msitprogram.net>
2021-08-19 11:31:16 +0530

    passed

--- 01-matrixmultiply-Python/matrixmultiply.py
+++ 01-matrixmultiply-Python/matrixmultiply.py
@@ -4,8 +4,38 @@
 # two matrices cannot be multiplied for any reason.
 
 
+def read2DArray():
+    a = []
+    l = int(input())
+    for i in range(l):
+        s = input().split(" ")
+        t = []
+        for j in range(len(s)):
+            t.append(int(s[j]))
+        a.append(t)
+    return a
+
 def fun_matrixmultiply(m1, m2):
-    return None
+    if (isirregularlist(m1) or isirregularlist(m2)):
+        return None
+    result=[]
+    for i in range(len(m1)): 
+        p=[]
+        for j in range(len(m2[0])): 
+            r=0
+            for k in range(len(m2)): 
+                r += (m1[i][k] * m2[k][j])
+            p.append(r)
+        result.append(p)
+    return result
+
+def isirregularlist(a):
+    max=len(a[0])
+    for i in a:
+        if(max!=len(i)):
+            return True
+    return False
+
 
 
 
