ba0b4e37968cd96e7e80aafb10f6357937cc20ce
neerajakoli <neeraja.kolisetty@msitprogram.net>
2021-08-26 13:16:22 +0530

    passed

--- 12-matrixadd-Python/matrixadd.py
+++ 12-matrixadd-Python/matrixadd.py
@@ -1,7 +1,9 @@
 # matrixAdd(L, M)[10 pts]
-# Background: we can think of a 2d list in Python as a matrix in math. To add two matrices, L and M, they must have 
+# Background: we can think of a 2d list in Python as a matrix in math. To add two 
+# matrices, L and M, they must have 
 # the same dimensions. 
-# Then, we loop over each row and col, and the result[row][col] is just the sum of L[row][col] and M[row][col]. For example:
+# Then, we loop over each row and col, and the result[row][col] is just the 
+# sum of L[row][col] and M[row][col]. For example:
 # L = [ [1,  2,  3],
 #       [4,  5,  6] ]
 # M = [ [21, 22, 23],
@@ -9,12 +11,30 @@
 # N = [ [1+21, 2+22, 3+23],
 #       [4+24, 5+25, 6+26]]
 # assert(matrixAdd(L, M) == N)
-# With this in mind, write the function matrixAdd(L, M) that takes two rectangular 2d lists (that we will consider 
+# With this in mind, write the function matrixAdd(L, M) that takes two 
+# rectangular 2d lists (that we will consider 
 # to be matrices) that you 
-# may assume only contain numbers, and returns a new 2d list that is the result of adding the two matrices. Return 
+# may assume only contain numbers, and returns a new 2d list that is the 
+# result of adding the two matrices. Return 
 # None if the two matrices 
 # cannot be added because they are of different dimensions.
 
 def matrixadd(L, M):
-	# Your code goes here
-	pass
+	if(len(L)!=len(M) or len(L[0])!=len(M[0])):
+		return None 
+	for i in range(len(L)):
+		if(len(L[i])!=len(M[i])):
+			return None
+	f=[] 
+	for i in range(len(L)):
+		t=[]
+		for j in range(len(L[0])): 
+			t.append(0)
+		f.append(t)
+	print(f)
+	for i in range(len(L)):
+		for j in range(len(L[0])):
+			p=0
+			f[i][j]=L[i][j]+M[i][j] 
+	return f
+print(matrixadd([[1,  2,  3],[4,  5,  6]], [[21, 22, 23], [24, 25, 26]]))
