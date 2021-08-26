83f98fd9275ff1d1dc9b01b7630254bd27f30e5b
neerajakoli <neeraja.kolisetty@msitprogram.net>
2021-08-26 12:20:52 +0530

    passed

--- 10-issorted-Python/issorted.py
+++ 10-issorted-Python/issorted.py
@@ -1,10 +1,18 @@
 # isSorted(a) (10 Pts)
-# Write the function isSorted(a) that takes a list of numbers and returns True if the list 
-# is sorted (either smallest-first or largest-first) and False otherwise. Your function 
-# must only consider each value in the list once (so, in terms of big-oh, which we will 
-# learn soon, it runs in O(n) time, where n=len(a)), and so in particular you may not sort 
+# Write the function isSorted(a) that takes a list of numbers 
+# and returns True if the list 
+# is sorted (either smallest-first or largest-first) and 
+# False otherwise. Your function 
+# must only consider each value in the list once (so, in 
+# terms of big-oh, which we will 
+# learn soon, it runs in O(n) time, where n=len(a)), and 
+# so in particular you may not sort 
 # the list.
 
 def issorted(a):
-	# your code goes here
-	pass
+	if(len(a)==3):
+		return a[1]-a[1]==a[2]-a[1]
+	for i in range(1,len(a)-1):
+		if(a[i]-a[i-1]!=a[i+1]-a[i]):
+			return False 
+	return True 
