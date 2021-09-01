021d3897e02d3f4eb513fa8690ec1f30c8d6980b
neerajakoli <neeraja.kolisetty@msitprogram.net>
2021-09-01 17:18:05 +0530

    passed

--- 05-nth_lychrelnumber-Python/nthlychrelnumber.py
+++ 05-nth_lychrelnumber-Python/nthlychrelnumber.py
@@ -5,6 +5,41 @@
 # The first few Lychrel numbers are 196, 295, 394, 493, 592, 689, 691, 788, 790, 879, 887….
 
 
+def reverse(num):
+    rnum = 0
+    while(num>0):
+        x = num%10
+        rnum = rnum*10+x
+        num = num//10
+    return rnum
+
+def check(num):
+    rnum = reverse(num)
+    if(rnum==num):
+        return True
+    else:
+        return False
+
 def nthlychrelnumbers(n):
-	# your code goes here
-	pass
+    # your code goes here
+    num = 196
+    count = 0
+    while(count<n):
+        num1 = num
+        for i in range(25):
+            number = num1 + reverse(num1)
+            val = check(number)
+            if(val):
+                break
+            else:
+                num1= number
+                
+        if(val==False):
+            count= count+1
+            num = num+1
+        else:
+            num = num+1
+
+                
+            
+    return num-1
