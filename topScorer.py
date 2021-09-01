e4c47d0550ea740b40de35a04cae14c4d64a69bf
neerajakoli <neeraja.kolisetty@msitprogram.net>
2021-09-01 11:46:31 +0530

    passed

--- topScorer.py
+++ topScorer.py
@@ -13,11 +13,34 @@
 # in the same order they appeared in the original data. 
 # If nobody wins (there is no data), return None (not the 
 # string "None"). So, for example:
-
 def topScorer(data):
     # Your code goes here...
-    return ""
-
+    if data == "":
+        return None
+    l = []
+    data = data.splitlines()
+    for i in range (len(data)):
+        data[i] = data[i].split(",")
+    for i in range (len(data)):
+        val = 0
+        for j in range (len(data[i])):
+            if j == 0:
+                l.append(data[i][j])
+            else:
+                val += int(data[i][j])
+        l.append(val)
+    val = 0
+    output = ''
+    for i in range (1,len(l),2):
+        if l[i] > val:
+            val = l[i]
+            output = l[i-1]
+        elif l[i] == val:
+            output += ","+ l[i-1]
+        i += 2
+        if i > len(l):
+            break
+    return output
 data = '''\
 Fred,10,20,30,40
 Wilma,10,20,30
