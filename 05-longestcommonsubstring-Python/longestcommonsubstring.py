3cd54243291e0d27c9b4f4a59813941fb0c72f1c
neerajakoli <neeraja.kolisetty@msitprogram.net>
2021-09-02 10:40:20 +0530

    passed

--- 05-longestcommonsubstring-Python/longestcommonsubstring.py
+++ 05-longestcommonsubstring-Python/longestcommonsubstring.py
@@ -6,7 +6,28 @@
 # If there are two or more longest common substrings, return the lexicographically smaller one (ie, just use "<" to 
 # compare the strings). So, for example:
 # longestCommonSubstring("abcABC", "zzabZZAB") returns "AB" and not "ab"
-
 def longestcommonsubstring(s1, s2):
     # Yourcode goes here
-    pass
+    s = ""
+    count = 0
+    l = []
+    for i in range(len(s1)):        
+        if s1[i] not in s2:
+            s = ""
+            continue
+        s += s1[i]
+        if s not in s2:
+            s = s[1:]
+        if s not in s2:
+            continue
+        if len(s)>=count:
+            count = len(s)
+            l.append(s)
+    cl = [i for i in l if len(i)==count]
+    if cl==[]:
+        return ""
+    lcse = "" + cl[0]
+    for i in cl:
+        if i<lcse:
+            lcse = i
+    return lcse
