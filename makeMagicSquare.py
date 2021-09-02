f15e67d56398474d217606e677f1080c32d11943
neerajakoli <neeraja.kolisetty@msitprogram.net>
2021-09-02 11:07:45 +0530

    passed

--- makeMagicSquare.py
+++ makeMagicSquare.py
@@ -34,5 +34,39 @@
 # column position is n, the new position would be: (0, n-2). 
 
 def makeMagicSquare(n):
-    # Your code goes here...
-    pass
+    magicSquare = [[0 for x in range(n)]
+                   for y in range(n)]
+    i = n / 2
+    j = n - 1
+    num = 1
+    while num <= (n * n):
+        if i == -1 and j == n:  # 3rd condition
+            j = n - 2
+            i = 0
+        else:
+            if j == n:
+                j = 0
+            if i < 0:
+                i = n - 1
+ 
+        if magicSquare[int(i)][int(j)]:  # 2nd condition
+            j = j - 2
+            i = i + 1
+            continue
+        else:
+            magicSquare[int(i)][int(j)] = num
+            num = num + 1
+ 
+        j = j + 1
+        i = i - 1  # 1st condition
+    print("Sum of each row or column",
+          n * (n * n + 1) / 2, "\n")
+ 
+    for i in range(0, n):
+        for j in range(0, n):
+            print('%2d ' % (magicSquare[i][j]),
+                  end='')
+            if j == n - 1:
+                print()
+n = 7
+print(makeMagicSquare(n))
